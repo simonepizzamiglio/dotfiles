830bb303527dcd1c3711cd40da303145e8dfe7d6
simonepizzamiglio <simonepizzamiglio93@gmail.com>
2022-03-02 08:46:43 +0100

    update apps list

--- apps.sh
+++ apps.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
 # apps
-brew cask install iterm2
-brew cask install spectacle
-brew cask install google-chrome
-brew cask install visual-studio-code
-brew cask install alfred
-brew cask install spotify
-brew cask install flux
-brew cask install gimp
-brew cask install malwarebytes
-brew cask install sourcetree
-brew cask install spark
-brew cask install telegram
-brew cask install figma
-brew cask install slack
-brew cask install appcleaner
+brew install --cask iterm2
+brew install --cask rectangle
+brew install --cask google-chrome
+brew install --cask visual-studio-code
+brew install --cask raycast
+brew install --cask spotify
+brew install --cask flux
+brew install --cask gimp
+brew install --cask malwarebytes
+brew install --cask sourcetree
+brew install --cask spark
+brew install --cask telegram
+brew install --cask figma
+brew install --cask slack
+brew install --cask appcleaner
+brew install --cask fig
+brew install --cask gitify
+brew install --cask postman
